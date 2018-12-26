6f916aa2ea2480edf8070c9c2b2ef60bf16a7a21
imiq09 <imiqbalanjum1998@gmail.com>
2018-12-26 17:59:50 +0530

    Initial commit

---
+++ app/src/main/java/com/imiqbalanjum1998/asus_pc/testalpp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.imiqbalanjum1998.asus_pc.testalpp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
